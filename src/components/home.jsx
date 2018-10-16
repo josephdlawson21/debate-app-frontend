2ef4ac05a690984dec6f903f97fc4b5a7b78944d
josephdlawson21 <josephdlawson21@gmail.com>
2018-10-16 18:21:20 -0400

    set up axios calls and list data

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
+import axios from '../utils/axiosconfig.js';
 
 class Home extends Component {
+  state = {
+    schools: [],
+    users: [],
+    tournaments: []
+  }
+
+  componentDidMount() {
+    axios.get('/schools')
+    .then((res) => {
+      console.log("lol", res);
+      this.setState({
+        schools: res.data
+      });
+    })
+
+    axios.get('/users')
+    .then((res) => {
+      console.log("lol", res);
+      this.setState({
+        users: res.data
+      });
+    })
+
+    axios.get('/tournaments')
+    .then((res) => {
+      console.log("lol", res);
+      this.setState({
+        tournaments: res.data
+      });
+    })
+  }
+
   render() {
     return (
       <div className="Home">
         <div>
-          username
+          <h1>users</h1>
+          <ol>
+            {this.state.users.map(userObj => <li>{userObj.name}</li>)}
+          </ol>
+        </div>
+        <div>
+          <h1>schools</h1>
+          <ol>
+            {this.state.schools.map(schoolObj => <li>{schoolObj.name}</li>)}
+          </ol>
         </div>
         <div>
           rounds
         </div>
         <div>
-          tournaments
+          <h1>tournaments</h1>
+          <ol>
+            {this.state.tournaments.map(tournamentObj => <li>{tournamentObj.name}</li>)}
+          </ol>
         </div>
       </div>
     );
--- src/utils/axiosconfig.js
+++ src/utils/axiosconfig.js
@@ -1,15 +1,17 @@
 const axios 									= require('axios');
 axios.defaults.headers.common['Authorization'] 	= `Bearer ${localStorage.getItem('token') || null}`;
 
-module.exports = {
-  	client: axios.create({
-		baseURL: '',
-		responseType: 'json',
-		timeout: 5000,
-  	}),
-  	cdnUrl: '',
-  	url: 'https://debate-app-backend.herokuapp.com/api/v1',
-  	setToken: () => {
-		  axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token') || null}`;
-  	},
-};
+// module.exports = {
+//   	url: 'https://debate-app-backend.herokuapp.com/api/v1',
+//   	setToken: () => {
+// 		  axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token') || null}`;
+//   	},
+// };
+
+ let client =  axios.create({
+  baseURL: 'https://debate-app-backend.herokuapp.com/api/v1',
+  responseType: 'json',
+  timeout: 5000,
+})
+
+export default client
